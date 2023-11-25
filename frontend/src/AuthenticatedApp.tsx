ff2fa4cc3760f343135824cfea09edf149693071
Padena Rasouli <padenarasouli@gmail.com>
2023-11-25 16:08:26 -0500

    map popover

--- frontend/src/AuthenticatedApp.tsx
+++ frontend/src/AuthenticatedApp.tsx
@@ -10,20 +10,25 @@ function AuthenticatedApp() {
       <div className="App" style={{overflowX: "hidden" }}>
       <header className="App-header">
         <nav className='navbar fixed-top navbar-expand-lg' style={{backgroundColor: "#F8FDF0"}}>
+
           <Link to={'/app'} className='navbar-brand mx-3'><img src={logo} style={{height: "50px"}}/></Link>
-          <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown' aria-controls='navbarNavDropdown' aria-expanded='false' aria-label='Toggle navigation'>
-            <span className='navbar-toggler-icon'></span>
-          </button>
-          <div className='collapse navbar-collapse' id='navbarNavDropdown'>
-            <ul className='navbar-nav'>
-              <li className='nav-item'>
-                <Link to={'/app'} className='nav-link'>Home</Link>
-              </li>
-              <li className='nav-item'>
-                <Link to={'/app/profile'} className='nav-link'>Profile</Link>
-              </li>
-            </ul>
-          </div>
+        
+        {/**
+          <ul className="navbar-nav d-flex flex-row me-1">
+          <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle" type="button" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false" aria-haspopup="true" > 
+                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                </svg>
+                </button>
+                <div className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                  <a href='/sign-in' className="dropdown-item">Log out</a>
+                </div>
+            </div>
+          </ul>
+          */}
+
         </nav>
       </header>
       <Outlet />
--- frontend/src/pages/MapPage.tsx
+++ frontend/src/pages/MapPage.tsx
@@ -1,9 +1,16 @@
 import Map from "../components/Map"
+
+
 function MapPage () {
     return (
-
+        <div className="pt-3">
+            <div className="input-group flex-nowrap mb-2 mt-5">
+                <span className="input-group-text" id="addon-wrapping">🔍</span>
+                <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="addon-wrapping"/>
+            </div>
             <Map />
-
+            
+        </div>
     )
 }
 export default MapPage;
