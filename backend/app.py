cbd2b36690a8e0d47e833e302c5616cbe5566b81
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2023-11-25 18:50:16 -0500

    add location routes to app

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from user_routes import user_routes
 from language_routes import language_routes
+from location_routes import location_routes
 
 app = Flask(__name__)
 app.register_blueprint(user_routes)
 app.register_blueprint(language_routes)
+app.register_blueprint(location_routes)
 
 
 @app.route("/test")
