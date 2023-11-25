75ad870524e1ec61644d4a1d521c38397b208d1d
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2023-11-25 14:48:52 -0500

    add language blueprint

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from user_routes import user_routes
+from language_routes import language_routes
 
 app = Flask(__name__)
 app.register_blueprint(user_routes)
+app.register_blueprint(language_routes)
 
 
 @app.route("/test")
