ae58d6c8880bce88ab1a1c22e6c1de3372e545ab
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2023-11-25 19:26:21 -0500

    fix merge conflict

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,13 @@ from flask import Flask
 from user_routes import user_routes
 from language_routes import language_routes
 from location_routes import location_routes
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(user_routes)
 app.register_blueprint(language_routes)
 app.register_blueprint(location_routes)
+CORS(app)
 
 
 @app.route("/test")
