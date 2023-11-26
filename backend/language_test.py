85fb311c097cf050cc99a391322e1148bbdc598f
Padena Rasouli-Baghban <padenarasouli@gmail.com>
2023-11-26 00:11:32 -0500

    add test

---
+++ backend/language_test.py
@@ -0,0 +1,23 @@
+import pytest
+import json
+from app import app
+import random
+
+
+def test_langs_route():
+    lang_response = app.test_client().post(
+        '/lang',
+        data=json.dumps({
+            "name": "Language",
+        }),
+        headers={"Content-Type": "application/json"})
+
+    assert lang_response.status_code == 201
+    lang_id = json.loads(lang_response.data.decode('utf-8')).get('id')
+    assert lang_id is not None
+
+    response = app.test_client().get(
+        '/langs',
+        headers={"Content-Type": "application/json"})
+    assert response.status_code == 201
+    assert json.loads(response.data.decode('utf-8')).get('name') is "Language"
