a0b6567a4e4e6e61a52638ea316375b2bb83b651
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2023-11-26 03:44:57 -0500

    add route for avg rating for location

--- backend/rating_routes.py
+++ backend/rating_routes.py
@@ -39,3 +39,18 @@ def ratings():
             return {"error": "error creating, try again"}, 400
         else:
             return {"id": id_data[0]}, 201
+
+
+@rating_routes.route('/avg_rating', methods=['GET'])
+def avg_rating():
+    if request.method == 'GET':
+        args = request.args
+        location_id = args.get("locationId", type=int)
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT AVG(r.rating) FROM locations l LEFT JOIN ratings r on l.id = r.location_id WHERE l.id = %s GROUP BY l.id;",
+            (location_id, ))
+        rating_data = cursor.fetchone()
+        if (rating_data is None) or (len(rating_data) < 1):
+            return {"error": "rating not available, try again later"}, 400
+        return {"avg_rating": round(rating_data[0], 2)}, 200
