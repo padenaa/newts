f7ccb4db840b61bbdc3c189940b9dc39cb885b68
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2023-11-25 19:54:28 -0500

    use new post helper method for register

--- frontend/src/components/CreateAcct.tsx
+++ frontend/src/components/CreateAcct.tsx
@@ -1,34 +1,22 @@
-
 import { BaseSyntheticEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
-interface AccountInfo {
-    user: string,
-    pass: string,
-    lang: number
-}
+import { postData } from "../helper";
 
 function CreateAcctComp () {
-    //const navigate = useNavigate();
+    const navigate = useNavigate();
     const [user, setUser] = useState("")
     const [pass, setPass] = useState("")
     const [lang, setLang] = useState(-1)
     const onSubmit = () => {
-        console.log(user, pass, lang)
-        fetch("http://127.0.0.1:5000/register", {
-             method: "POST",
-             body: JSON.stringify({
+        postData("http://127.0.0.1:5000/register", {
                  username: user,
                  password: pass,
-                 language: lang
-             }),
-             headers: {
-                 "Content-type": "application/json; charset=UTF-8",
-                 "Access-Control-Allow-Origin": "*",
-             }
-         }).then((res) => console.log(res));
-        //let resJson = await response.json();
-        //localStorage.setItem('id',resJson?.id);
-        //navigate('/app', { replace: true });
+                 language: lang,
+        }).then((res: any) => {
+            console.log(res)
+            localStorage.setItem('id', res.id);
+            navigate('/app', { replace: true });
+        });
      }
 
     const onUsernameChange = (data: BaseSyntheticEvent) => {
@@ -41,7 +29,7 @@ function CreateAcctComp () {
         setLang(data?.target?.value)
     }
     return (
-        <form className='text-start'>
+        <div className='text-start'>
             <h5 className="card-title">Create your Newts account</h5>
                 <div className="form-floating mb-3">
                     <input type="text" className="form-control is-lgreen" id="floatingInput" placeholder="john.doe" onChange={onUsernameChange}/>
@@ -53,13 +41,13 @@ function CreateAcctComp () {
                 </div>
                 <select className="form-select mb-3" aria-label="Default select example" onChange={onLanguageChange} defaultValue="0">
                     <option value="0" disabled={true}>Choose language</option>
-                    <option value="1">spanish</option>
+                    <option value="1">Spanish</option>
                     <option value="2">British</option>
                     <option value="3">Australian</option>
                 </select>
 
             <button className="button" type="button" onClick={onSubmit}>Create Account</button>
-        </form>
+        </div>
     )
 }
 
